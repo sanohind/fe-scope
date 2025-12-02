05c4d727b4f59c54718907d2ae45a0be18ff1f91
Algie <konohamaru497@gmail.com>
2025-12-02 14:51:36 +0700

    ADD: LOGO SANOH

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -257,13 +257,13 @@ const AppSidebar: React.FC = () => {
         <Link to="/">
           {isExpanded || isHovered || isMobileOpen ? (
             <>
-              <img className="dark:hidden" src="/images/logo/logo.svg" alt="Logo" width={150} height={40} />
-              <img className="hidden dark:block" src="/images/logo/logo-dark.svg" alt="Logo" width={150} height={40} />
+              <img className="dark:hidden" src="/images/logo/Logo-sanoh-2.png" alt="Logo" width={150} height={40} />
+              <img className="hidden dark:block" src="/images/logo/Logo-sanoh-2-white.png" alt="Logo" width={150} height={40} />
             </>
           ) : (
             <>
-              <img className="dark:hidden" src="/images/logo/logo-icon.svg" alt="Logo" width={32} height={32} />
-              <img className="hidden dark:block" src="/images/logo/logo-icon-dark.svg" alt="Logo" width={32} height={32} />
+              <img className="dark:hidden" src="/images/logo/SANOH-LITE-01.png" alt="Logo" width={32} height={32} />
+              <img className="hidden dark:block" src="/images/logo/SANOH-LITE-02.png" alt="Logo" width={32} height={32} />
             </>
           )}
         </Link>
